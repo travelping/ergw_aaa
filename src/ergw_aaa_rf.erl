dc7e5bf46305ed49d179ad558e5f9391e99955fc
Andreas Schultz <andreas.schultz@travelping.com>
2019-09-10 16:12:18 +0200

    have DIAM Rf and NASREQ initialize Acct-Interim-Interval
    DIAMETER Rf and NASREQ should take the default value for Acct-Interim-Interval
    from the sesion global setting. This was lost in a recent change. This commits
    restores that behavior.

--- src/ergw_aaa_rf.erl
+++ src/ergw_aaa_rf.erl
@@ -237,11 +237,21 @@ inc_number(Key, Session) ->
     {Number, ?set_svc_opt(Key, Number, Session)}.
 
 %% to_session/3
-to_session(Procedure, SessEvs, Avps) ->
-    maps:fold(to_session(Procedure, _, _, _), SessEvs, Avps).
+to_session(Procedure, {Session0, Events0}, Avps) ->
+    Session =
+	case Session0 of
+	    #{'Acct-Interim-Interval' := Interim}
+	      when not is_map_key('Acct-Interim-Interval', Avps),
+		   is_integer(Interim), Interim > 0 ->
+		Session0#{'Acct-Interim-Interval' => [Interim]};
+	    _ ->
+		Session0
+	end,
+    maps:fold(to_session(Procedure, _, _, _), {Session, Events0}, Avps).
 
 %% to_session/4
-to_session(_, 'Acct-Interim-Interval', [Interim], {Session, Events}) ->
+to_session(_, 'Acct-Interim-Interval', [Interim], {Session, Events})
+  when is_integer(Interim), Interim > 0 ->
     Trigger = ergw_aaa_session:trigger(?APP, 'Offline', periodic, Interim),
     {Session, ergw_aaa_session:ev_set(Trigger, Events)};
 to_session(_, _, _, SessEv) ->
