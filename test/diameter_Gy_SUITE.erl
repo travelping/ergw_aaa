5ba77d7a7cd4456fc7254036ca31b11cf87010d7
Andreas Schultz <andreas.schultz@travelping.com>
2021-05-25 12:11:25 +0200

    make the session init more asynchronous
    The session init phase can take significant time. This would cause
    a backlog in the session supervisor when many sessions where started
    at once. This becomes visible as a significant grows of the mailbox
    of the supervisor process.
    
    Also increase the outstanding requests limit for the rate limit
    test. The inproved startup time also means the traffic in that
    test will be much more bursty and will run into that limit.

--- test/diameter_Gy_SUITE.erl
+++ test/diameter_Gy_SUITE.erl
@@ -64,7 +64,7 @@
 
 -define(CONFIG,
 	#{rate_limits =>
-	      #{default => #{outstanding_requests => 1, rate => 10}},
+	      #{default => #{outstanding_requests => 10, rate => 10}},
 	  functions => ?DIAMETER_FUNCTION,
 	  handlers =>
 	      #{ergw_aaa_static => ?STATIC_CONFIG,
