8cc66bf3899343a63024f121dc1a5556603ec449
Ferenc Holzhauser <ferenc.holzhauser@travelping.com>
2021-08-31 09:48:20 +0200

    Improve session state check in CT

--- test/ergw_aaa_test_lib.hrl
+++ test/ergw_aaa_test_lib.hrl
@@ -27,6 +27,6 @@
 -import(ergw_aaa_test_lib, [meck_init/1, meck_reset/1, meck_unload/1, meck_validate/1,
 			    set_cfg_value/3, get_cfg_value/2,
 			    get_stats/1, diff_stats/2, wait_for_diameter/2,
-			    outstanding_reqs/0, get_session_stats/0, wait_for_session/4,
+			    outstanding_reqs/0, get_session_stats/0, get_session_stats/2, wait_for_session/4,
 			    reset_session_stats/0]).
 -endif.
